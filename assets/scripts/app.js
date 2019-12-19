0a189d1f4f80667a742b870253b6d9925926d192
BenLirio <benjlirio@gmail.com>
2019-12-19 16:27:57 -0500

    Add sidebar

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,11 @@
 'use strict'
 import { showInvoiceTable } from './invoice/ui'
 import { initAuth } from './auth/events'
+import { showSideBar } from './nav/ui'
 import store from './store'
 $(() => {
   initAuth()
+  showSideBar()
   $.ajax({
     url: 'http://localhost:4741/invoices',
     method: 'GET',
