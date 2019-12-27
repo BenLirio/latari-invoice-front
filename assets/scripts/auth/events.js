3aa03033953502016dba8268f0a416ff3b271b64
BenLirio <benjlirio@gmail.com>
2019-12-27 18:03:22 -0500

    Now shows the response upon error sign-in

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -9,7 +9,7 @@ import { init as navInit } from '../nav/events'
 
 const events = new Events()
 if(!autoSignIn()) {
-showSignIn()
+  showSignIn()
 }
 
 events.listenToButton('modal', '#sign-in-modal .sign-up-btn', onClickSignUpBtn)
@@ -46,9 +46,10 @@ function onClickSignOutBtn(e) {
 function onSignIn(data) {
   signIn(data)
     .then(res => {
-      authenticated(res.user)
+      authenticate(res.user)
+      clearModal()
     })
-  clearModal()
+    .catch(failedSignIn)
 }
 function onSignUp(data) {
   signUp(data).then( () => {
@@ -80,23 +81,23 @@ function autoSignIn() {
     }
   }
   if (user) {
-    authenticated(user)
+    authenticate(user)
     return true
   }
   return false
 }
 
-function authenticated(user) {
+function authenticate(user) {
   storeUser(user)
   invoiceInit()
   navInit()
 }
 
+export function failedSignIn(e) {
+  $('#sign-in-modal form input[type="password"]').addClass('is-invalid')
+  $('#sign-in-modal form input').val('')
+}
 
-// import { clearModal } from './ui'
-
-// import { initNav } from '../nav/events'
-// import { clearMain } from '../invoice/ui'
 export function initAuth() {
   let user
   const cookie = document.cookie
@@ -117,108 +118,3 @@ export function initAuth() {
     showSignIn()
   }
 }
-
-// // submit sign-up
-// import { signUp } from './api'
-// /* import { clearModal } from './ui' */
-// /* import { signUp } from './api' */
-// /* import { showSignIn } from './ui' */
-// $('#modal').on('submit','#sign-up-form', e => {
-//   e.preventDefault()
-//   const target = e.target
-//   const data = getFormFields(target)
-  
-//   signUp(data)
-//   .then(()=>showSignIn())
-//   .catch(console.error)
-// })
-
-// /* import { showSignIn } from './ui' */
-// $('#modal').on('click', '#sign-up-form-cancel', e => {
-//   showSignIn()
-// })
-
-// // submit sign-in
-// /* import { initNav } from '../nav/events' */
-// import { init as initInvoice } from '../invoice/events'
-// import { signIn } from './api'
-// /* import { clearModal } from './ui' */
-// $('#modal').on('submit','#sign-in-form', e => {
-//   e.preventDefault()
-//   const target = e.target
-//   const data = getFormFields(target)
-
-//   const getUser = response => {
-//     return response.user
-//   }
-//   const storeUser = function(user) {
-//     store.user = user
-//     return store.user
-//   }
-//   const stringifyUser = function(pojoUser) {
-//     return JSON.stringify(pojoUser)
-//   }
-//   const setCookie  = function(jsonUser) {
-//     document.cookie = jsonUser
-//     return jsonUser
-//   }
-//   signIn(data)
-//     .then(getUser)
-//     .then(storeUser)
-//     .then(stringifyUser)
-//     .then(setCookie)
-//     .then(() => {
-//       clearModal()
-//       initNav()
-//       initInvoice()
-//     })
-//     .catch(console.error)
-// })
-
-// // press sign-up
-// import { showSignUp } from './ui'
-// $('#modal').on('click', '#sign-up-btn', () => {
-//   showSignUp()
-// })
-
-// // press change-password
-// import { showChangePassword } from './ui'
-// $('#header').on('click', '#change-password-btn', () => {
-//   showChangePassword()
-// })
-
-// // submit change-password
-// /* import { clearModal } from './ui' */
-// import { changePassword } from './api'
-// events.listenToForm('modal', 'change-password', data => {
-//   changePassword(data)
-// })
-// $('#modal').on('submit','#change-password-form', e => {
-//   e.preventDefault()
-//   const target = e.target
-//   const data = getFormFields(target)
-//   changePassword(data).then(console.log)
-// })
-
-// /* import { clearModal } from './ui' */
-// $('#modal').on('click', '#change-password-cancel', e => {
-//   clearModal()
-// })
-
-// // press log-out
-// import { signOut } from './api'
-// /* import { showSignIn } from './ui' */
-// import { endNav } from '../nav/events'
-// $('#header').on('click', '#sign-out-btn', () => {
-//   const clearCookies = function() {
-//     document.cookie = ''
-//     return document.cookie
-//   }
-//   signOut()
-//     .then(clearCookies)
-//     .then(()=>{
-//       showSignIn()
-//       endNav()
-//       clearMain()
-//     })
-// })
